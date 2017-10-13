e6f3b5201f9abb02d8feb497fa29fb3b3b5f396b
Joel Bernasky <joel_bernasky@homedepot.com>
2017-10-13 10:58:28 -0400

    examples from object module

---
+++ ObjectsMakeTheWorldGoRound/program.js
@@ -0,0 +1,12 @@
+var dice = require("./dice");
+var die = dice.die;
+
+console.log(dice);
+
+die.size = 10;
+console.log(die.roll());
+console.log(die.roll());
+console.log(die.roll());
+console.log("Total rolls " + die.totalRolls);
+
+console.log(die);
