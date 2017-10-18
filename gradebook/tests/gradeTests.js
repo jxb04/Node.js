7336474c43fd1c89f97dafca3741689ebbf26779
Joel Bernasky <joel_bernasky@homedepot.com>
2017-10-18 11:05:38 -0400

    add additional unit test

--- gradebook/tests/gradeTests.js
+++ gradebook/tests/gradeTests.js
@@ -5,6 +5,55 @@ exports["setUp"]=function(callback) {
       callback();
 };
 
+exports["Can compute a letter grade of A"] = function(test){
+      book.addGrade(100);
+      book.addGrade(90);
+
+      var result = book.getLetterGrade();
+
+      test.equal(result, 'A');
+      test.done();
+};
+
+exports["Can compute a letter grade of B"] = function(test){
+      book.addGrade(80);
+      book.addGrade(80);
+
+      var result = book.getLetterGrade();
+
+      test.equal(result, 'B');
+      test.done();
+};
+
+exports["Can compute a letter grade of C"] = function(test){
+      book.addGrade(79.9);
+
+      var result = book.getLetterGrade();
+
+      test.equal(result, 'C');
+      test.done();
+};
+
+exports["Can compute a letter grade of D"] = function(test){
+      book.addGrade(40);
+      book.addGrade(80);
+
+      var result = book.getLetterGrade();
+
+      test.equal(result, 'D');
+      test.done();
+};
+
+exports["Can compute a letter grade of A"] = function(test){
+      book.addGrade(100);
+      book.addGrade(0);
+
+      var result = book.getLetterGrade();
+
+      test.equal(result, 'F');
+      test.done();
+};
+
 exports["Can average grades"] = function(test) {
       book.addGrade(100);
       book.addGrade(50);
