1b5a0333efe60c0478232b00cda942af6309588d
Joel Bernasky <joel_bernasky@homedepot.com>
2017-10-19 11:25:54 -0400

    added express.js and created web interface to gradebook application

--- gradebook/program.js
+++ gradebook/program.js
@@ -1,7 +1,25 @@
 var book = require("./lib/grades").book;
+var express = require("express");
+var app = express();
 
-for (var i = 2; i < process.argv.length; i++) {
-    book.addGrade(parseInt(process.argv[i]));
-}
+app.get("/", function(req, res) {
+    res.send("Hello, World!");
+});
 
-console.log(book.getAverage());
+app.get("/grade", function(req, res){
+
+// "1,2,3".split(",") => ["1","2","3"];
+    var grades = req.query.grades.split(",");
+    for(var i = 0; i < grades.length; i++) {
+        book.addGrade(parseInt(grades[i]));
+    }
+
+    var average = book.getAverage();
+    var letter = book.getLetterGrade();
+
+    res.send("Your average is " + average + " grade " + letter);
+
+});
+
+app.listen(3000);
+console.log("Server ready...");
